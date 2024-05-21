145fd7a61c80df5cb277597a69e7e1aa7d4fe7fb
ramiro.dev <mirolima021@gmail.com>
2024-05-21 18:28:10 -0300

    operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+
+        double soma = 15.8 + 13.75;
+        int subtracao = 50 - 34;
+        double resultado = (10 * 5) + (75 / 5);
+        int divisao = 1000 / 72;
+        double modulo = 150 % 2;
+
+        String linguagem = "LINGUAGEM" + " " + "JAVA";
+        String nomeCompleto = "Ramiro" +  " " + "Lima" + " " + "Gonçalves";
+
+        
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(resultado);
+        System.out.println(divisao);
+        System.out.println(modulo);
+        System.out.println(nomeCompleto);
+        System.out.println(linguagem);
+    }
+}
