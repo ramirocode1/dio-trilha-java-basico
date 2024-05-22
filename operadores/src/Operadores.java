769fc171248f57eea61b62e53c7cdc7074696c68
ramiro.dev <mirolima021@gmail.com>
2024-05-22 18:55:27 -0300

    Operadores

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -3,20 +3,50 @@ public class Operadores {
 
         double soma = 15.8 + 13.75;
         int subtracao = 50 - 34;
-        double resultado = (10 * 5) + (75 / 5);
+        double resultado1 = (10 * 5) + (75 / 5);
         int divisao = 1000 / 72;
         double modulo = 150 % 2;
 
         String linguagem = "LINGUAGEM" + " " + "JAVA";
         String nomeCompleto = "Ramiro" +  " " + "Lima" + " " + "Gonçalves";
-
-        
         System.out.println(soma);
         System.out.println(subtracao);
-        System.out.println(resultado);
+        System.out.println(resultado1);
         System.out.println(divisao);
         System.out.println(modulo);
         System.out.println(nomeCompleto);
         System.out.println(linguagem);
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        // String resultado = " ";
+        // if (a==b) 
+        //    resultado = "VERDADEIRO";
+        // else
+        //    resultado = "FALSO";
+
+        String resultado = a==b ? "VERDADEIRO" : "FALSO"; // Abreviação o IF, com operadores alternario
+
+        System.out.println(resultado);
+
+
+        int numero1 = 2;
+        int numero2 = 3;
+        double numero3 = 15.5;
+        double numero4 = 20.0;
+
+        boolean simNao = numero1 != numero2; //True
+        boolean simNao2 = numero3 == numero4; // False
+
+        System.out.println("Nº1 é diferente do Nº2?" + " " +  simNao);
+        System.out.println("Nº3 é igual do Nº4?" + " " + simNao2);
+    
+        // Diferente !=
+        // Igual ==
+        // Maior, Maior ou Igual, Menor, Menor ou igual = >, >= / <, <=
+            
+        
     }
 }
