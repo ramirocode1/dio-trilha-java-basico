e51878f151fe142060a7caba51ac819ac930d3e4
ramiro.dev <mirolima021@gmail.com>
2024-06-18 20:53:05 -0300

    .

--- desafio-de-codigo-simulacao-bancaria/src/SimulacaoBancaria.java
+++ desafio-de-codigo-simulacao-bancaria/src/SimulacaoBancaria.java
@@ -4,54 +4,51 @@ public class SimulacaoBancaria {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         double saldo = 0;
         boolean continuar = true;
 
         while (continuar) {
-            System.out.println("Escolha uma opção:");
-            System.out.println("1: Depositar");
-            System.out.println("2: Sacar");
-            System.out.println("3: Consultar Saldo");
-            System.out.println("0: Encerrar");
+
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.print("Digite o valor a ser depositado: ");
-                    double deposito = scanner.nextDouble();
-                    if (deposito > 0) {
-                        saldo += deposito;
-                        System.out.printf("Saldo atual: %.1f%n", deposito);
-                    } else {
-                        System.out.println("Valor de depósito inválido.");
-                    }
+                    // Depositar
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.println("Saldo atual: " + saldo);
                     break;
                     
                 case 2:
-                   System.out.print("Digite o valor a ser sacado: ");
+                    // Sacar
                     double saque = scanner.nextDouble();
-                    if (saque > 0 && saque <= saldo) {
+                    if (saque <= saldo) {
                         saldo -= saque;
-                        System.out.printf("Saldo atual: %.1f%n", saldo);
-                    } else if (saque > saldo) {
+                        System.out.println("Saldo atual: " + saldo);
+                    }
+                    
+                    else {
                         System.out.println("Saldo insuficiente.");
-                    } else {
-                        System.out.println("Valor de saque inválido.");
                     }
                     break;
                     
                 case 3:
-                    System.out.printf("Saldo atual: %.1f%n", saldo);
+                    // Consultar Saldo
+                    System.out.println("Saldo atual: " + saldo);
                     break;
+                    
                 case 0:
+                    // Encerrar
                     System.out.println("Programa encerrado.");
-                    continuar = false;  
+                    continuar = false;
                     break;
                     
                 default:
+                    // Opção inválida
                     System.out.println("Opção inválida. Tente novamente.");
             }
-        }   
-        scanner.close();   
+        }
+        scanner.close();
     }
 }
